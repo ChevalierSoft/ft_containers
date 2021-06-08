66168c1930e1f6e470d953a43bd52d411c6f27dc
ChevalierSoft <ibanarashi@outlook.fr>
2021-06-08 02:26:26 +0200

    preparing vector. might be easyer than list to start with

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -1,29 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/08 01:19:28 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/06/08 02:25:37 by dait-atm         ###   ########.fr       */
+/*   Created: 2021/06/08 02:23:18 by dait-atm          #+#    #+#             */
+/*   Updated: 2021/06/08 02:25:18 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
 #include <iostream>
-#include <algorithm>
-#include <list>
-#include <vector>
 
-#include "../includes/list.hpp"
-#include "../includes/vector.hpp"
-
-
-int	main(void)
+namespace ft
 {
-	ft::vector<int>		v;
-	std::vector<int>	rv;
-	
+	template <typename T>
+	class vector
+	{
+	private:
+		/* data */
+	public:
+		// Constructors & Destructors
+		vector(void) {};
+		// vector(/* args */);
+		~vector(void) {};
 
-	return (0);
+		// Copy constructor
+		vector(const vector<T> & copy);
+
+		// Operation overload =
+		vector<T> &	operator=(const vector<T> &	copy);
+	};
 }
+
+#endif
