1b31c73d9df083dd317e185f6ef7345378ef2e54
ChevalierSoft <ibanarashi@outlook.fr>
2021-07-08 01:31:31 +0200

    list is not mendatory anymore

--- main.cpp
+++ main.cpp
@@ -6,16 +6,14 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 01:19:28 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/07/08 01:24:32 by dait-atm         ###   ########.fr       */
+/*   Updated: 2021/07/08 01:31:18 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <algorithm>
-#include <list>
 #include <vector>
 
-#include "./list/list.hpp"
 #include "./vector/vector.hpp"
 
 #include "./utils/color.h"
