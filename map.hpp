0df0cc5e0750f8c37dcedd7fdea03cbcc11b989a
ChevalierSoft <ibanarashi@outlook.fr>
2021-10-20 05:46:52 +0200

    before the Tree, adding BST_Node. renamed Tree in BinarySearchTree.

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 21:38:23 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/10/20 02:32:06 by dait-atm         ###   ########.fr       */
+/*   Updated: 2021/10/20 05:45:24 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ namespace ft
 {
 	template< class Key, class T, class Compare = std::less<Key>,
 				class Allocator = std::allocator<std::pair<const Key, T> > >
-	class map /// * ___________________________________________________________
+	class map /// * ___________________________________________________________  map
 	{
 	public:
 		typedef Key										key_type;
@@ -51,50 +51,95 @@ namespace ft
 		typedef ft::reverse_iterator<iterator>			reverse_iterator;
 		typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 	
-	protected:
-		class Tree // * _________________________
+	// protected:
+		// ? BinarySearchTree is an implementation of bst
+		class BinarySearchTree // ? ___________________________________________  BinarySearchTree
 		{
-			// * Constructors & Destructors
+		// protected:
+			// ? BST_Node is a node used in BinarySearchTree
+			struct BST_Node // ! _______________________________________________  BST_Node
+			{
+				// ! Constructors & Destructors _______________________________
+
+				// ! default (1)
+				BST_Node() : content(), parent(NULL), left(NULL), right(NULL) {}
+
+				// ! default with initialisation (2)
+				BST_Node(const_reference val, BST_Node* p = NULL, BST_Node* l = NULL, BST_Node* r = NULL) : content(val), parent(p), left(l), right(r) {}
+
+				// ! copy (3)
+				BST_Node(const BST_Node & copy) : content(copy.content), parent(copy.parent), left(copy.left), right(copy.right) {}
+
+				~BST_Node() {}
+
+				// ! Operators ________________________________________________
+				BST_Node &		operator= (const BST_Node & rhs)
+				{
+					if (this != &rhs)
+					{
+						content = rhs.content;
+						parent = rhs.parent;
+						left = rhs.left;
+						right = rhs.right;
+					}
+					return (*this);
+				}
+
+				bool			operator==(const BST_Node & rhs)
+				{
+					return (content == rhs.content);
+				}
+
+				// ! Variables ________________________________________________
+				value_type		content;
+				BST_Node*		parent;		// parent node
+				BST_Node*		left;		// left node
+				BST_Node*		right;		// right node
+
+			};  // ! BST_Node _________________________________________________  
+
+			// ? Constructors & Destructors ___________________________________
 		public:
-			Tree() : _node(NULL), _left(NULL), _right(NULL) {}
-			Tree(pointer rhs) : _node(rhs), _left(NULL), _right(NULL) {}
-			~Tree() {	this->clear();	}
+			BinarySearchTree()  {}
 
-			void	clear()
-			{
-				_node->~value_type();
-				_node = NULL;
-				_left = NULL;
-				_right = NULL;
-			}
+			// BinarySearchTree(pointer rhs) : _node(rhs), _left(NULL), _right(NULL) {}
+			
+			~BinarySearchTree() {}	//	this->clear();	}
 
-			void	replace(pointer rhs)
-			{
-				_node->~value_type();
-				_node = rhs;
-			}
+			// void		clear()
+			// {
+			// 	_node->~value_type();
+			// 	_node = NULL;
+			// 	_left = NULL;
+			// 	_right = NULL;
+			// }
 
-			// * variables ______________________
+			// void		replace(pointer rhs)
+			// {
+			// 	_node->~value_type();
+			// 	_node = rhs;
+			// }
+
+			// ? Variables ____________________________________________________
 		protected:
-			pointer		_node;		// node data
-			Tree*		_left;		// left leaf
-			Tree*		_right;		// right leaf
-		};
+			BST_Node	_root;		// node data
+			
+		}; // ? BinarySearchTree ______________________________________________
 
 		/// * Constructors & Destructors ______________________________________
 
 	public:
 		// ? empty (1)
-		explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) {}
+		explicit map (const key_compare& comp = key_compare(), const allocator_type& alloc = allocator_type()) : _comp(comp), _allocator(alloc) {}
 
 		// ? range (2)
-		template <class InputIterator>
-		map (InputIterator first, InputIterator last,
-			const key_compare& comp = key_compare(),
-			const allocator_type& alloc = allocator_type());
+		// template <class InputIterator>
+		// explicit map (InputIterator first, InputIterator last,
+		// 	const key_compare& comp = key_compare(),
+		// 	const allocator_type& alloc = allocator_type());
 
 		// ? copy (3)
-		map (const map& x);
+		// map (const map& x);
 
 		~map(void) {}
 
@@ -115,7 +160,9 @@ namespace ft
 		/// * Variables _______________________________________________________
 	 	// ? Maps are typically implemented as balanced binary trees.
 	private:
-		Tree	_root;
+		BinarySearchTree			_root;
+		key_compare		_comp;
+		allocator_type	_allocator;
 
 	}; /// * map ______________________________________________________________
 
