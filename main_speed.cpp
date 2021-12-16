b5a78bd66fdd857073d9c3f482654b44ab9e8c92
ChevalierSoft <ibanarashi@outlook.fr>
2021-12-16 17:15:10 +0100

    redone speed tester. now it is displaying the real elapsed time.

--- main_speed.cpp
+++ main_speed.cpp
@@ -6,11 +6,13 @@
 /*   By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 01:54:58 by dait-atm          #+#    #+#             */
-/*   Updated: 2021/12/03 04:12:33 by dait-atm         ###   ########.fr       */
+/*   Updated: 2021/12/16 17:11:18 by dait-atm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <chrono>
+#include <ctime>
 #ifdef ORIGINAL
 	namespace ft = std;
 	#include <map>
@@ -21,9 +23,9 @@
 #endif
 
 #define MAP_CELLS		1000000
-#define VECTOR_CELLS	1000000
+#define VECTOR_CELLS	100000
 
-#define __DEB(s) std::cerr<<s<<std::endl;
+#define __DEB(s) std::cout<<s<<std::endl;
 
 class	menfou
 {
@@ -42,23 +44,36 @@ public:
 int		menfou::_nb = 0;
 std::ostream &	operator<< (std::ostream & o, const menfou & m) { o << "obj : " << m.c << " " << m.b << std::endl;	return o; }
 
+void	display_elapsed_time(	std::chrono::time_point<std::chrono::system_clock> start,
+								std::chrono::time_point<std::chrono::system_clock> end )
+{
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "elapsed : " << elapsed_seconds.count() << std::endl;
+}
+
 void	speed_map()
 {
 	//__________________________________________________________________________
 	std::cout <<std::endl<< "___ speed_map ___" << std::endl;
 
-	ft::map<int, std::string> m;
+	std::chrono::time_point<std::chrono::system_clock>	start;
+	ft::map<int, std::string>							m;
 
 	__DEB("insert ()");
-	for (size_t i = 0; i < MAP_CELLS; ++i)
+	start = std::chrono::system_clock::now();
 	{
-		m.insert(ft::make_pair<int, std::string>(rand(), "🥷"));
+		for (size_t i = 0; i < MAP_CELLS; ++i)
+			m.insert(ft::make_pair<int, std::string>(rand(), "🥷"));
 	}
+	display_elapsed_time(start, std::chrono::system_clock::now());
+
 	__DEB("erase ()");
-	for (auto im = m.begin(); im != m.end(); im = m.begin())
+	start = std::chrono::system_clock::now();
 	{
-		m.erase(im);
+		for (auto im = m.begin(); im != m.end(); im = m.begin())
+			m.erase(im);
 	}
+	display_elapsed_time(start, std::chrono::system_clock::now());
 }
 
 void	speed_vector()
@@ -67,20 +82,29 @@ void	speed_vector()
 	std::cout <<std::endl<< "___ speed_vector ___" << std::endl;
 
 	ft::vector<menfou> v;
+	std::chrono::time_point<std::chrono::system_clock> start;
 
 	__DEB("push_back()");
-	for (size_t i = 0; i < VECTOR_CELLS; ++i)
-		v.push_back(menfou());
+	start = std::chrono::system_clock::now();
+	{
+		for (size_t i = 0; i < VECTOR_CELLS; ++i)
+			v.push_back(menfou());
+	}
+	display_elapsed_time(start, std::chrono::system_clock::now());
 
 	__DEB("erase()");
-	for (auto im = v.begin(); im != v.end(); im = v.begin())
-		v.erase(im);
+	start = std::chrono::system_clock::now();
+	{
+		for (auto im = v.begin(); im != v.end(); im = v.begin())
+			v.erase(im);
+	}
+	display_elapsed_time(start, std::chrono::system_clock::now());
 }
 
 int	main(void)
 {
 	srand(time(NULL));
-	// speed_vector();
+	speed_vector();
 	speed_map();
 	__DEB("done");
 	return (0);
--- speed.sh
+++ speed.sh
@@ -6,7 +6,7 @@
 #    By: dait-atm <dait-atm@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/19 02:07:27 by dait-atm          #+#    #+#              #
-#    Updated: 2021/11/19 02:07:33 by dait-atm         ###   ########.fr        #
+#    Updated: 2021/12/16 16:58:50 by dait-atm         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,7 @@
 
 g++ main_speed.cpp -o ft_speed.out
 g++ main_speed.cpp -D ORIGINAL=1 -o std_speed.out
-time ./ft_speed.out
-time ./std_speed.out
+./ft_speed.out > logs/ft_speed.log
+./std_speed.out > logs/std_speed.log
+diff ./logs/ft_speed.log ./logs/std_speed.log
 rm ft_speed.out std_speed.out
